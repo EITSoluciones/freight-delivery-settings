01dadab122a3050c17e5aa220e8b11f9a4a4ad95
Miguel Cauich <miguel.cauich@itsoft.mx>
2025-11-18 10:09:26 -0600

    fix: :bug: Include tailwind correctly
    Include tailwind correctly

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -51,13 +51,16 @@
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="expiration_date">
                         Expiration Date
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="expiration_date" name="expiration_date" type="date" placeholder="Expiration Date" value="{{ old('expiration_date') }}">
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="expiration_date" name="expiration_date"
+                    type="date" placeholder="Expiration Date"
+                    value="{{ old('expiration_date') }}">
                     @error('expiration_date')
                         <p class="text-red-500 text-xs italic">{{ $message }}</p>
                     @enderror
                 </div>
-                <div class="flex items-center justify-between">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                <div class="flex justify-end">
+                    <button class="w-full sm:w-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                         Create Client
                     </button>
                 </div>
