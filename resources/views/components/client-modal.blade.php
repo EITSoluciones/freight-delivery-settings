585b4bd030e9858b7e0fc756be3d2ef5c432c6f7
Miguel Cauich <miguel.cauich@itsoft.mx>
2025-11-19 13:19:04 -0600

    perf: :zap: Improve labels
    Improve labels

--- resources/views/components/client-modal.blade.php
+++ resources/views/components/client-modal.blade.php
@@ -103,7 +103,7 @@
 
                 <button type="submit"
                     class="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none w-full mb-3">
-                    {{ $isEdit ? 'Update Client' : 'Create Client' }}
+                    {{ $isEdit ? 'Update ' : 'Create ' }}
                 </button>
             </form>
         </div>
