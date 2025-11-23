81c3a7e6ee18f4eede17322c48d54b0f598c64ec
Miguel Cauich <miguel.cauich@itsoft.mx>
2025-11-23 09:32:18 -0600

    fix: :bug: Login style fixed
    Login style fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class=" flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+<body class=" flex p-6 lg:p-8 items-center justify-center min-h-screen flex-col">
     <div
         class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
         <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row ">
@@ -24,7 +24,7 @@
                     @csrf
                     <div class="mb-4">
                         <label for="email"
-                            class="block text-sm font-medium text-gray-700 dark:text-gray-300">Email</label>
+                            class="block text-sm font-medium text-gray-300">Email</label>
                         <input id="email" type="email" name="email" value="{{ old('email') }}" required
                             autofocus
                             class="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
@@ -34,7 +34,7 @@
                     </div>
                     <div class="mb-4">
                         <label for="password"
-                            class="block text-sm font-medium text-gray-700 dark:text-gray-300">Password</label>
+                            class="block text-sm font-medium text-gray-300">Password</label>
                         <input id="password" type="password" name="password" required
                             class="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
                     </div>
@@ -45,7 +45,7 @@
                 </form>
             </div>
             <div
-                class="relative lg:-ml-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg  w-full lg:w-[438px] shrink-0 overflow-hidden">
+                class=" h-64 relative lg:h-auto lg:-ml-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg  w-full lg:w-[438px] shrink-0 overflow-hidden">
                 <img src="{{ asset('assets/img/auth-screens.jpg') }}"
                     class="absolute inset-0 w-full h-full object-cover" alt="Fondo de login">
             </div>
